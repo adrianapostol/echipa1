6de648f4af23c732497c30b9acc3f98ac2c8c9d4
unknown <radu.macovei@P5347.svdomain1.softvision.ro>
2015-07-18 20:41:04 +0300

    Removed all access if not logged in

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -58,6 +58,6 @@ class AppController extends Controller
 
     public function beforeFilter(Event $event)
     {
-        $this->Auth->allow(['view', 'index']);
+        //$this->Auth->allow(['view', 'index']);
     }
 }
